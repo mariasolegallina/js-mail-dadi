09ea3077e22a8b33e82dfdd63835416fa0e753ff
mariasolegallina <gallinamariasole@gmail.com>
2023-10-09 15:41:13 +0200

    add - true false

--- js/email.js
+++ js/email.js
@@ -1,7 +1,27 @@
 console.log("Controllo della mail")
 
 const emailUtente = prompt ('Scrivi la tua mail')
-console.log(emailUtente)
+// console.log(emailUtente)
 
 const listaEmail = ['gianni@gianni.it', 'simona@simona.it', 'armando@armando.it', 'erica@erica.it', 'mattia@mattia.it']
-console.log(listaEmail)
+// console.log(listaEmail)
+
+const lunghezzaArray = listaEmail.length
+// console.log(lunghezzaArray)
+
+let indirizzoTrovato = false
+
+for (let i = 0; i < lunghezzaArray; i++) {
+    const emailInLista = listaEmail [i]
+    // console.log(i)
+
+    if (emailInLista === emailUtente) {
+        indirizzoTrovato = true
+    }
+}
+
+if (indirizzoTrovato === true) {
+    console.log("evivva evviva")
+} else {
+    console.log("ritenta")
+}
