de41a537d2089526ebfcb4dfa96541d28d52e175
mariasolegallina <gallinamariasole@gmail.com>
2023-10-09 14:33:42 +0200

    add - listaEmail

--- js/email.js
+++ js/email.js
@@ -1,4 +1,7 @@
 console.log("Controllo della mail")
 
-emailUtente = prompt ('Scrivi la tua mail')
+const emailUtente = prompt ('Scrivi la tua mail')
 console.log(emailUtente)
+
+const listaEmail = ['gianni@gianni.it', 'simona@simona.it', 'armando@armando.it', 'erica@erica.it', 'mattia@mattia.it']
+console.log(listaEmail)
